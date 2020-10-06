aff4d19731a1d1a93cc7c64ac9b159359a3b3bd7
ahky20 <ahky2000@hawaii.edu>
2020-10-05 18:23:09 -1000

    Update ex1.js

--- Lab9/ex1.js
+++ Lab9/ex1.js
@@ -6,8 +6,9 @@ var lastTwo = StrYear.slice(2,4);
 var step1 = Number(lastTwo); // step1=13
 var step2=parseInt(step1/4); // = 3
 var step3 = step2 + step1;   // = 16
-var step4 = 2;               // Not Jan, so look at month before on table
+var step4 = 0;               // Not Jan, so look at month before on table
 var step5 = step3 + day;     // Yes the month is Jan. doesn't apply in this situation
 var step6 = step4 + step3;   // = 18
 var step7 = step6 + day;
 var step8 = step7;
+var final = step8 % 7;
