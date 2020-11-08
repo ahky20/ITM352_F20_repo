b8f25e6d7b54d8bf30573aed2cf4b7c5761ae099
ahky20 <ahky2000@hawaii.edu>
2020-11-08 11:52:22 -1000

    finished(?) assignment1

--- Young_Amber_Assignment1/server.js
+++ Young_Amber_Assignment1/server.js
@@ -1,21 +1,21 @@
-// Bonus Exercise 5
+// File copied from Lab 13 Bonus Exercise 5
 var express = require('express');
 var app = express(); // object to initialize express
 var myParser = require("body-parser");
-var fs = require('fs');
 var data = require('./public/product_data.js');
 var products = data.products;
+var fs = require('fs');
 
 app.use(express.static('./public'));
 app.use(myParser.urlencoded({ extended: true }));
 
 function process_quantity_form (POST, response) {
     if (typeof POST['purchase_submit_button'] != 'undefined') {
-       var contents = fs.readFileSync('./views/display_quantities_template.view', 'utf8');
+       var contents = fs.openSync('./public/invoice.html', 'utf8');
        receipt = '';
        for(i in products) { 
         let q = POST[`quantity_textbox${i}`];
-        let model = products[i]['model'];
+        let model = products[i]['name'];
         let model_price = products[i]['price'];
         if (isNonNegInt(q)) {
           receipt += eval('`' + contents + '`'); // render template string
@@ -27,12 +27,14 @@ function process_quantity_form (POST, response) {
       response.end();
     }
  }
+ 
 
 function isNonNegInt(stringToCheck, returnErrors = false) {
     errors = []; // assume no errors at first
     if(Number(stringToCheck) != stringToCheck) errors.push('Not a number!'); // Check if string is a number value
     if(stringToCheck < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(stringToCheck) != stringToCheck) errors.push('Not an integer!'); // Check that it is an integer
+    
     return returnErrors ? errors : (errors.length == 0) 
 }
 
@@ -44,8 +46,8 @@ app.all('*', function (request, response, next) {
 app.post("/process_form", function (request, response) {
     console.log("Got POST process_form") // debug statement
     let POST = request.body;
-    process_quantity_form(POST,response);
+    process_quantity_form(POST, response);
+    
 });
 
-app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
-
+app.listen(8080, () => console.log(`listening on port 8080`)); 
