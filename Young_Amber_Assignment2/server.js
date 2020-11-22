c43cb71ffb5966214ff33f66d616171d8bc8fe71
ahky20 <ahky2000@hawaii.edu>
2020-11-21 16:00:51 -1000

    working on a2

--- Young_Amber_Assignment2/server.js
+++ Young_Amber_Assignment2/server.js
@@ -8,14 +8,140 @@ var fs = require('fs');
 var filename = "user_data.json";
 var queryString = require('query-string');
 
-
 app.use(express.static('./public'));
 app.use(myParser.urlencoded({ extended: true }));
+
+// register form validation code from w3resource
+function formValidation() {
+  var uid = document.registration.userid;
+  var passid = document.registration.passid;
+  var uname = document.registration.username;
+  var uadd = document.registration.address;
+  var ucountry = document.registration.country;
+  var uzip = document.registration.zip;
+  var uemail = document.registration.email;
+  var umsex = document.registration.msex;
+  var ufsex = document.registration.fsex; if (userid_validation(uid, 5, 12)) {
+    if (passid_validation(passid, 7, 12)) {
+      if (allLetter(uname)) {
+        if (alphanumeric(uadd)) {
+          if (countryselect(ucountry)) {
+            if (allnumeric(uzip)) {
+              if (ValidateEmail(uemail)) {
+                if (validsex(umsex, ufsex)) {
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+
+} function userid_validation(uid, mx, my) {
+  var uid_len = uid.value.length;
+  if (uid_len == 0 || uid_len >= my || uid_len < mx) {
+    alert("User Id should not be empty / length be between " + mx + " to " + my);
+    uid.focus();
+    return false;
+  }
+  return true;
+}
+function passid_validation(passid, mx, my) {
+  var passid_len = passid.value.length;
+  if (passid_len == 0 || passid_len >= my || passid_len < mx) {
+    alert("Password should not be empty / length be between " + mx + " to " + my);
+    passid.focus();
+    return false;
+  }
+  return true;
+}
+function allLetter(uname) {
+  var letters = /^[A-Za-z]+$/;
+  if (uname.value.match(letters)) {
+    return true;
+  }
+  else {
+    alert('Username must have alphabet characters only');
+    uname.focus();
+    return false;
+  }
+}
+function alphanumeric(uadd) {
+  var letters = /^[0-9a-zA-Z]+$/;
+  if (uadd.value.match(letters)) {
+    return true;
+  }
+  else {
+    alert('User address must have alphanumeric characters only');
+    uadd.focus();
+    return false;
+  }
+}
+function countryselect(ucountry) {
+  if (ucountry.value == "Default") {
+    alert('Select your country from the list');
+    ucountry.focus();
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+function allnumeric(uzip) {
+  var numbers = /^[0-9]+$/;
+  if (uzip.value.match(numbers)) {
+    return true;
+  }
+  else {
+    alert('ZIP code must have numeric characters only');
+    uzip.focus();
+    return false;
+  }
+}
+function ValidateEmail(uemail) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (uemail.value.match(mailformat)) {
+    return true;
+  }
+  else {
+    alert("You have entered an invalid email address!");
+    uemail.focus();
+    return false;
+  }
+} function validsex(umsex, ufsex) {
+  x = 0;
+
+  if (umsex.checked) {
+    x++;
+  } if (ufsex.checked) {
+    x++;
+  }
+  if (x == 0) {
+    alert('Select Male/Female');
+    umsex.focus();
+    return false;
+  }
+  else {
+    alert('Form Succesfully Submitted');
+    window.location.reload()
+    return true;
+  }
+}
+
+app.post("/process_order", function (request, response) {
+  console.log("Got POST process_order") // debug statement
+  let POST = request.body;
+  process_quantity_form(POST, response);
+  //response.redirect("/invoice.html");
+}
+);
 
 // function to process quantity form from lab13 info_server_ex4.js
 function process_quantity_form(POST, response) {
   if (typeof POST['purchase_submit_button'] != 'undefined') {
-    var contents = fs.open('./public/invoice.html', 'utf8');
+    //var contents = fs.open('./public/invoice.html', 'utf8');
     receipt = '';
     for (i in products) {
       let q = POST[`quantity${i}`];
@@ -29,18 +155,10 @@ function process_quantity_form(POST, response) {
         receipt += `<h3><font color="red">${q} is not a valid quantity for ${model}!</font></h3>`;
       }
     }
-    //response.send(receipt);
+    response.send(receipt);
     //response.end();
   }
 }
-
-app.post("/process_form", function (request, response) {
-  console.log("Got POST process_form") // debug statement
-  let POST = request.body;
-  process_quantity_form(POST, response);
-  response.redirect("/invoice.html");
-}
-);
 
 // check for invalid inputs such as negative values or non integers
 function isNonNegInt(stringToCheck, returnErrors = false) {
@@ -54,88 +172,13 @@ function isNonNegInt(stringToCheck, returnErrors = false) {
 
 // check for valid quantities in textbox
 function checkQuantityTextbox() {
-  errs = isNonNegInt(quantity_form.quantity_textbox.value, true);
-  quantity_textbox_message.innerHTML = errs.join(", ");
+  errs = isNonNegInt(order_form.quantity.value, true);
+  quantity.innerHTML = errs.join(", ");
 }
 
 app.all('*', function (request, response, next) {
   response.send(request.method + ' to path ' + request.path);
   next();
-});
-
-// code from lab14 ProcessLogin.js
-app.use(myParser.urlencoded({ extended: true }));
-if (fs.existsSync(filename)) {
-  data = fs.readFileSync(filename, 'utf-8'); // 'utf-8' makes data readable to user
-  //console.log("Success! We got: " + data)
-
-  user_data = JSON.parse(data);
-  console.log("User_data=", user_data);
-} else {
-  console.log("Sorry can't read file " + filename);
-}
-
-app.get("/login", function (request, response) {
-  // Give a simple login form
-  str = `
-<body>
-<form action="" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
-    `;
-  response.send(str);
-});
-
-app.post("/login", function (request, response) {
-  // Process login form POST and redirect to logged in page if ok, back to login page if not
-  console.log("Got a POST login request");
-  POST = request.body;
-  user_name_from_form = POST["username"];
-  console.log("User name from form=" + user_name_from_form);
-  if (user_data[user_name_from_form] != undefined) {
-    response.send(`<H3> User ${POST[username]} logged in`);
-  } else {
-    response.send(`Sorry Buddy`)
-  }
-
-  app.get("/register", function (request, response) {
-    // Give a simple register form
-    str = `
-    <body>
-    <form action="" method="POST">
-    <input type="text" name="username" size="40" placeholder="enter username" ><br />
-    <input type="password" name="password" size="40" placeholder="enter password"><br />
-    <input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
-    <input type="email" name="email" size="40" placeholder="enter email"><br />
-    <input type="submit" value="Submit" id="submit">
-    </form>
-    </body>
-        `;
-    response.send(str);
-  });
-
-  app.post("/register", function (request, response) {
-    // process a simple register form
-    POST = request.body;
-    console.log("Got register POST");
-    if (POST["username" != undefined] && POST['password' != undefined] && POST['repeat_password' != undefined]) { // Validate user input
-      username = POST["username"];
-      user_data[username] = {};
-      user_data[username].name = username;
-      user_data[username].password = POST['password'];
-      user_data[username].email = POST['email'];
-
-      data = JSON.stringify(user_data);
-      fs.writeFileSync(filename, data, "utf-8");
-
-      response.send("User " + username + " logged in");
-    } else {
-      response.send("Sorry, try again");
-    }
-  });
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`)); 
